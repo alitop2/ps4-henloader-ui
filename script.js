030344eaed468a7d35b494723b64fbabf54a8bf9
alitop2 <alitop369@gmail.com>
2026-09-14 03:59:56 +0300

    Change HOST URL to relative path

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const activate = document.getElementById("activate");
-const HOST = "https://w90008.github.io/11/g2all/index.html";
+const HOST = "./g2all/index.html";
 activate.addEventListener("click", () => { window.location.href = HOST; });
